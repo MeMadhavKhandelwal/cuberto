73b81078a242bbcc49c280c13ab9c7e9251a995d
memkh <khandelwalmadhav5@gmail.com>
2023-10-31 20:01:43 +0530

    Uploading all files

---
+++ script.js
@@ -0,0 +1,41 @@
+
+Shery.mouseFollower();
+Shery.makeMagnet(".magnet");
+Shery.hoverWithMediaCircle(".hoverVideo", {videos: ["./0.mp4", "./2.mp4", "./3.mp4"]});
+
+gsap.to(".elem", {
+    scrollTrigger: {
+        trigger: "#fimages",
+        pin: true,
+        start: "top top",
+        end: "bottom bottom",
+        endTrigger: ".last",
+        scrub: 1,
+    },
+    y: "-300%",
+    ease: Power1,
+});
+let sections = document.querySelectorAll(".elem");
+// Shery.imageEffect(".images", {
+//   style: 1,
+// //   config: { onMouse: { value: 1 } },
+//   slideStyle: (setScroll) => {
+//     sections.forEach(function (section, index) {
+//       ScrollTrigger.create({
+//         trigger: section,
+//         start: "top top",
+//         scrub: 1,
+//         onUpdate: function (prog) {
+//           setScroll(prog.progress + index);
+//         },
+//       });
+//     });
+//   },
+// });
+Shery.imageEffect(".images", {
+    style: 3,
+    scrollSnapping: true,
+    scrollSpeed: 6,
+    touchSpeed: 6,
+    damping: 7,
+  });
